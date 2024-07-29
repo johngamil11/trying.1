4a44351675b765c1ef8285fa78fff55b9361401c
Kimo Store <johngamil52@gmail.com>
2024-07-29 21:06:35 +0200

    Changed floating action location.

--- lib/main.dart
+++ lib/main.dart
@@ -59,6 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
